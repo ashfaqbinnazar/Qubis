8f00f8e2a0189d7cbb3ff0a4a8d5ad5c18c5ec61
Ashfaq <ashfaqbinnazar@yahoo.com>
2018-12-03 15:52:18 +0530

    till now

--- app/src/main/java/com/example/hi/school/TeacherAttendance4.java
+++ app/src/main/java/com/example/hi/school/TeacherAttendance4.java
@@ -1,13 +1,28 @@
 package com.example.hi.school;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class TeacherAttendance4 extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
 
+public class TeacherAttendance4 extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_attendance4);
+
+        ArrayList<String> years = new ArrayList<String>();
+        int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = 1900; i <= thisYear; i++) {
+            years.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, years);
+
+        Spinner spinYear = (Spinner)findViewById(R.id.spinnerYear);
+        spinYear.setAdapter(adapter);
     }
 }
